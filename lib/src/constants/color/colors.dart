c879ed4d08aa21fea8e84b839342945e8c94cba5
princenyamadi <=>
2023-09-12 05:51:06 +0000

    register app colors

---
+++ lib/src/constants/color/colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class GAColors {
+  static const grey200 = Color(0xFFF0F0F0);
+  static const grey400 = Color(0xFFA7A7A7);
+  static const grey450 = Color(0xFF808080);
+  static const grey600 = Color(0xFF656565);
+  static const grey700 = Color(0xFF575757);
+  static const grey800 = Color(0xFF1F1F1F);
+  static const iconColour = Color(0xFF656565);
+  static const systemGray40 = Color(0xFFE1E5E8);
+  static const white = Color(0xFFFFFFFF);
+  static const primary = Color(0xFF22A87B);
+  static const primaryLight = Color(0xFF90A59E);
+  static const primaryDark = Color(0xFF2E413B);
+  static const background = Color(0xFFF7F7F7);
+}
