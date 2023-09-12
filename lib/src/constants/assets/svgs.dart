f47c97c1df1c23f8f6ec33aaf8022c5c7f54d699
princenyamadi <=>
2023-09-12 05:49:50 +0000

    register svg assets

---
+++ lib/src/constants/assets/svgs.dart
@@ -0,0 +1,10 @@
+class Svgs {
+  static const home = 'assets/svg/home.svg';
+  static const settings = 'assets/svg/setting.svg';
+  static const user = 'assets/svg/user.svg';
+  static const search = 'assets/svg/search-normal.svg';
+  static const notification = 'assets/svg/notification.svg';
+  static const star = 'assets/svg/star.svg';
+  static const backButton = 'assets/svg/backbutton.svg';
+  static const profile2user = 'assets/svg/profile-2user.svg';
+}
